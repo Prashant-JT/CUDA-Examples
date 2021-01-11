d71e0d8fa8f8b012d3e2c8889e5d55911fb2a980
JoseMAP-99 <55493484+JoseMAP-99@users.noreply.github.com>
2021-01-11 18:04:59 +0000

    Add files via upload

--- MATLAB/cuda.m
+++ MATLAB/cuda.m
@@ -1,6 +1,7 @@
 clear; clc;
 % uno;
 % cuatro;
+seis;
 
 function uno
     a = (0:99);
@@ -16,9 +17,23 @@ function cuatro
     
     plot(x, y);  
     grid();
-    xlabel('TamaÃ±o de la matriz cuadrada');
+    xlabel('Tamaño de la matriz cuadrada');
     ylabel('Tiempo empleado en segundos');
-    title('Producto escalar de matrices cuadradas');
+    title('Producto escalar de matrices cuadradas (Secuencial)');    
+end
+
+function seis
+    x = 2.^(0:5);
+    y_cuda = [0.000004, 0.000004, 0.000004, 0.000005, 0.000010, 0.000080];
+    
+    y_seq = [0.0, 0.0, 0.00000002, 0.00000012, 0.00000114, 0.00001285];
+    
+    plot(x, y_cuda, x, y_seq);  
+    grid();
+    xlabel('Tamaño de la matriz cuadrada');
+    ylabel('Tiempo empleado en segundos');
+    title('Comparativa del producto escalar de matrices cuadradas');    
+    legend('CUDA', 'Secuencial', 'Location', 'NorthWest');
 end
 
 
