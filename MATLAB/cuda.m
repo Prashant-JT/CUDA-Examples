d8c2e49186c5e15dd5a44b1498aa50d05c605ccd
JoseMAP-99 <55493484+JoseMAP-99@users.noreply.github.com>
2021-01-15 15:57:36 +0000

    Add files via upload

--- MATLAB/cuda.m
+++ MATLAB/cuda.m
@@ -1,7 +1,8 @@
 clear; clc;
 % uno;
-% cuatro;
-seis;
+cuatro;
+% seis;
+% ocho;
 
 function uno
     a = (0:99);
@@ -13,11 +14,12 @@ end
 
 function cuatro
     x = 2.^(0:5);
-    y = [0.0, 0.0, 0.00000002, 0.00000012, 0.00000114, 0.00001285];
+    y = [0.00000001, 0.00000003, 0.00000017, 0.00000112, 0.00000865, 0.00007434];
     
     plot(x, y);  
     grid();
     xlabel('Tamaño de la matriz cuadrada');
+    ylim([-1*10^-5 9*10^-5]);
     ylabel('Tiempo empleado en segundos');
     title('Producto escalar de matrices cuadradas (Secuencial)');    
 end
@@ -26,13 +28,29 @@ function seis
     x = 2.^(0:5);
     y_cuda = [0.000004, 0.000004, 0.000004, 0.000005, 0.000010, 0.000080];
     
-    y_seq = [0.0, 0.0, 0.00000002, 0.00000012, 0.00000114, 0.00001285];
+    y_seq = [0.00000001, 0.00000003, 0.00000017, 0.00000112, 0.00000865, 0.00007434];
     
     plot(x, y_cuda, x, y_seq);  
     grid();
     xlabel('Tamaño de la matriz cuadrada');
     ylabel('Tiempo empleado en segundos');
-    title('Comparativa del producto escalar de matrices cuadradas');    
+    ylim([-1*10^-5 9*10^-5]);
+    title('Comparativa del producto escalar de matrices cuadradas (1..32)');    
+    legend('CUDA', 'Secuencial', 'Location', 'NorthWest');
+end
+
+function ocho
+    x = 2.^(5:10);
+    y_cuda = [0.000702, 0.001355, 0.006762, 0.048501, 0.376575, 3.033569];
+    
+    y_seq = [0.0000800, 0.00054000, 0.00471000, 0.05905000, 0.46531000, 6.28000000];
+    
+    plot(x, y_cuda, x, y_seq);  
+    grid();
+    xlabel('Tamaño de la matriz cuadrada');
+    ylabel('Tiempo empleado en segundos');
+    ylim([-1 6.5]);
+    title('Comparativa del producto escalar de matrices cuadradas (32..1024)');    
     legend('CUDA', 'Secuencial', 'Location', 'NorthWest');
 end
 
